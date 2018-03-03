1c1ac1c497cae59214da172872bcf7d484fd63c2
Kamil <blueflamesxcode@gmail.com>
2018-03-03 10:29:33 +0100

    initialize repository

---
+++ src/main/java/com/webapp/effectiveness/EffectivenessImproverApplication.java
@@ -0,0 +1,12 @@
+package com.webapp.effectiveness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EffectivenessImproverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EffectivenessImproverApplication.class, args);
+	}
+}
