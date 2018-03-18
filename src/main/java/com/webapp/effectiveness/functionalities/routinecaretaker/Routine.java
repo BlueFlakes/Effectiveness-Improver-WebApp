ed8aac8d80ecd36acd2e40a4330a5687a5a85f56
Kamil <blueflamesxcode@gmail.com>
2018-03-18 01:43:23 +0100

    Add routine caretaker family :)

---
+++ src/main/java/com/webapp/effectiveness/functionalities/routinecaretaker/Routine.java
@@ -0,0 +1,31 @@
+package com.webapp.effectiveness.functionalities.routinecaretaker;
+
+import com.webapp.effectiveness.common.datastructures.circularlinkekdlist.CircularList;
+
+import java.util.Objects;
+
+public class Routine {
+    private final CircularList<Cycle> cycles;
+
+    {
+        this.cycles = CircularList.createCircularArrayList();
+    }
+
+    public void addCycle(Cycle cycle) {
+        Objects.requireNonNull(cycle);
+        this.cycles.add(cycle);
+    }
+
+    public void removeCycle(Cycle cycle) {
+        Objects.requireNonNull(cycle);
+        this.cycles.remove(cycle);
+    }
+
+
+    public static void main(String[] args) {
+        Routine routine = new Routine();
+
+
+
+    }
+}
