5ec10929aa119e34047846fd165646f7f4ade8f1
Kamil <blueflamesxcode@gmail.com>
2018-03-18 09:22:37 +0100

    rename interface name to more proper one

--- src/main/java/com/webapp/effectiveness/common/timeUtils/TimeUtils.java
+++ src/main/java/com/webapp/effectiveness/common/timeUtils/TimeUtils.java
@@ -3,16 +3,16 @@ package com.webapp.effectiveness.common.timeUtils;
 public enum  TimeUtils {
     ; // sign end of fields
 
-    public interface CalculatePattern {
+    public interface CalculateTimeTransition {
         long apply(long quantity);
     }
 
-    public enum ToMillis implements CalculatePattern {
+    public enum ToMillis implements CalculateTimeTransition {
         FROM_MINUTES {
             @Override
             public long apply(long quantity) {
                 return (60 * 1000) * quantity;
             }
-        };
+        }
     }
 }
