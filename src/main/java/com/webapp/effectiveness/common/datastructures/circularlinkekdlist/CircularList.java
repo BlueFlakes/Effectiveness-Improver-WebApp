e2c0444fe26d62c0e73b86f0b3e2ade370d91afd
Kamil <blueflamesxcode@gmail.com>
2018-03-18 01:03:12 +0100

    Add CircularList

---
+++ src/main/java/com/webapp/effectiveness/common/datastructures/circularlinkekdlist/CircularList.java
@@ -0,0 +1,50 @@
+package com.webapp.effectiveness.common.datastructures.circularlinkekdlist;
+
+import java.util.*;
+import java.util.function.Supplier;
+
+public class CircularList<T> {
+    private List<T> container;
+    private int indicator = 0;
+
+    private CircularList() {}
+
+    public static <T> CircularList<T> createCircularArrayList() {
+        CircularList<T> circularList = new CircularList<>();
+        circularList.setContainer(ArrayList::new);
+
+        return circularList;
+    }
+
+    public static <T> CircularList<T> createCircularLinkedList() {
+        CircularList<T> circularList = new CircularList<>();
+        circularList.setContainer(LinkedList::new);
+
+        return circularList;
+    }
+
+    private void setContainer(Supplier<List<T>> listSupplier) {
+        if (this.container != null)
+            throw new IllegalStateException("Shouldn't be invoked after creation.");
+
+        this.container = listSupplier.get();
+    }
+
+    public void add(T o) {
+        Objects.requireNonNull(o);
+        this.container.add(o);
+    }
+
+    public void remove(T o) {
+        Objects.requireNonNull(o);
+        this.container.remove(o);
+    }
+
+    public T next() {
+        if (this.container.isEmpty())
+            throw new EmptyContainerException();
+
+        int nextElementIndex = indicator++ % this.container.size();
+        return container.get(nextElementIndex);
+    }
+}
---
+++ src/main/java/com/webapp/effectiveness/common/datastructures/circularlinkekdlist/EmptyContainerException.java
@@ -0,0 +1,6 @@
+package com.webapp.effectiveness.common.datastructures.circularlinkekdlist;
+
+import com.webapp.effectiveness.baseclasses.ApplicationRuntimeException;
+
+public class EmptyContainerException extends ApplicationRuntimeException {
+}
