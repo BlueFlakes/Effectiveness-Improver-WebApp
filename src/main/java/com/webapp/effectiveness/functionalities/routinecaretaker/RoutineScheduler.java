084d662501bf19a4e75481eab358960f9abdb4aa
Kamil <blueflamesxcode@gmail.com>
2018-03-19 22:32:42 +0100

    update routinescheduler impl

--- src/main/java/com/webapp/effectiveness/functionalities/routinecaretaker/RoutineScheduler.java
+++ src/main/java/com/webapp/effectiveness/functionalities/routinecaretaker/RoutineScheduler.java
@@ -1,14 +1,16 @@
 package com.webapp.effectiveness.functionalities.routinecaretaker;
 
-import com.webapp.effectiveness.common.executorWrappers.ExecutorCachedThreadPoolWrapper;
+import com.webapp.effectiveness.common.ApplicationCloseable;
 import com.webapp.effectiveness.common.validators.ValidatorUtils;
 import com.webapp.effectiveness.functionalities.statistics.LifeSpan;
 import com.webapp.effectiveness.functionalities.task.Task;
 import com.webapp.effectiveness.functionalities.timebeater.ElapsedTimeBeater;
 
-public class RoutineScheduler implements Runnable {
-    private LifeSpan lifeSpan = new LifeSpan();
-    private ExecutorCachedThreadPoolWrapper threadPoolWrapper = ExecutorCachedThreadPoolWrapper.getInstance();
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class RoutineScheduler implements ApplicationCloseable {
+    private final ExecutorService executorService = Executors.newSingleThreadExecutor();
 
     private Routine routine;
     private ElapsedTimeBeater elapsedTimeBeater;
@@ -20,32 +22,62 @@ public class RoutineScheduler implements Runnable {
         this.routine = routine;
         this.elapsedTimeBeater = elapsedTimeBeater;
         this.task = task;
-
-        begin();
-    }
-
-    private void begin() {
-        this.threadPoolWrapper.execute(elapsedTimeBeater);
-    }
-
-    private void turnOnTaskExecution() {
-        this.elapsedTimeBeater.addSubscriber(this.lifeSpan);
-    }
-
-    private void turnOffTaskExecution() {
-        this.elapsedTimeBeater.removeSubscriber(this.lifeSpan);
     }
 
     @Override
-    public void run( ) {
-        while (true) {
-            Cycle currentCycle = this.routine.nextCycle();
-            CycleType cycleType = currentCycle.getCycleType();
+    public void onClose( ) {
+        this.executorService.shutdownNow();
+    }
 
-            if (currentCycle.isTimeExceeded(this.lifeSpan.getLength())
-                    && cycleType == CycleType.WORK_TIME) {
+    public CycleRunner runNextCycle() {
+        Cycle currentCycle = this.routine.nextCycle();
 
+        CycleRunner cycleRunner = new CycleRunner(currentCycle);
+        this.executorService.execute(cycleRunner);
 
+        return cycleRunner;
+    }
+
+    public class CycleRunner implements Runnable {
+        private LifeSpan lifeSpan = new LifeSpan();
+        private Cycle cycle;
+
+        public CycleRunner(Cycle cycle) {
+            this.cycle = cycle;
+        }
+
+        private long convertCurrentLifeSpanToMillis() {
+            return this.lifeSpan.getLength() * elapsedTimeBeater.getSleepingPeriodInMillis();
+        }
+
+        public long calculateHowManyMillisLeftToFinishCycle() {
+            long currentLifeSpanMillis = convertCurrentLifeSpanToMillis();
+            return this.cycle.calculatePeriodDiff(currentLifeSpanMillis);
+        }
+
+        public boolean isCycleFinished() {
+            long currentLifeSpanMillis = convertCurrentLifeSpanToMillis();
+            return this.cycle.isTimeExceeded(currentLifeSpanMillis);
+        }
+
+        @Override
+        public void run( ) {
+            try {
+                elapsedTimeBeater.addSubscriber(this.lifeSpan);
+                handleCycleExecution();
+
+            } catch (InterruptedException e) {
+                // not need any care
+            } finally {
+                elapsedTimeBeater.removeSubscriber(this.lifeSpan);
+            }
+        }
+
+        private void handleCycleExecution() throws InterruptedException {
+            boolean isTimeExceeded = false;
+            while (!isTimeExceeded) {
+                Thread.sleep(100);
+                isTimeExceeded = isCycleFinished();
             }
         }
     }
