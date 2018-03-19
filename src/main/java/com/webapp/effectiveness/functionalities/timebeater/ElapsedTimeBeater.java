46a997ebc9fcd84ef31ad941d90a1667116ac955
Kamil <blueflamesxcode@gmail.com>
2018-03-19 12:35:02 +0100

    Modify impl of ElapsedTimeBeater, ScheduledExecutor fit here more than Thread.sleep() which before let to destroy notify precision per period

--- src/main/java/com/webapp/effectiveness/functionalities/timebeater/ElapsedTimeBeater.java
+++ src/main/java/com/webapp/effectiveness/functionalities/timebeater/ElapsedTimeBeater.java
@@ -1,33 +1,58 @@
 package com.webapp.effectiveness.functionalities.timebeater;
 
+import com.webapp.effectiveness.common.ApplicationCloseable;
 import com.webapp.effectiveness.common.cacheutils.Cache;
 import com.webapp.effectiveness.common.observator.Subscriber;
 import com.webapp.effectiveness.common.observator.SubscribersEden;
+import com.webapp.effectiveness.common.validators.ValidatorUtils;
 
-public class ElapsedTimeBeater implements Runnable {
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Supplier;
+
+// Recommended is to test performance relation between amount of subscribers and sleeping period
+// If TimeBeater get too much subscribers with very low sleeping period
+// then he can fail to update every value
+public class ElapsedTimeBeater implements Runnable, ApplicationCloseable {
     private static final Cache<Long, ElapsedTimeBeater> elapsedTimeBeaterCache = new Cache<>(100);
+    private final ScheduledExecutorService timeBeat = Executors.newSingleThreadScheduledExecutor();
 
-    private final SubscribersEden subscribersEden = new SubscribersEden();
+    private final SubscribersEden subscribersEden;
     private final long sleepingPeriodInMillis;
 
-    private ElapsedTimeBeater(long sleepingPeriodInMillis) {
+    private ElapsedTimeBeater(long sleepingPeriodInMillis,
+                              Supplier<SubscribersEden> edenSupplier) {
+
         if (sleepingPeriodInMillis < 1)
             throw new IllegalArgumentException("Invalid period. Expect greater than 0");
 
         this.sleepingPeriodInMillis = sleepingPeriodInMillis;
+        this.subscribersEden = edenSupplier.get();
+        startTimeBeat();
     }
 
-    public static ElapsedTimeBeater createCached(long sleepingPeriodInMillis) {
+    private void startTimeBeat() {
+        timeBeat.scheduleAtFixedRate(this, 0, sleepingPeriodInMillis, TimeUnit.MILLISECONDS);
+    }
+
+    public static ElapsedTimeBeater createCached(long sleepingPeriodInMillis,
+                                                 Supplier<SubscribersEden> edenSupplier) {
+
+        ValidatorUtils.requireNonNull(edenSupplier);
         return elapsedTimeBeaterCache.create(sleepingPeriodInMillis,
-                                        () -> new ElapsedTimeBeater(sleepingPeriodInMillis));
+                                        () -> new ElapsedTimeBeater(sleepingPeriodInMillis, edenSupplier));
     }
 
-    public static ElapsedTimeBeater create(long sleepingPeriodInMillis) {
-        return new ElapsedTimeBeater(sleepingPeriodInMillis);
+    public static ElapsedTimeBeater create(long sleepingPeriodInMillis,
+                                           Supplier<SubscribersEden> edenSupplier) {
+
+        ValidatorUtils.requireNonNull(edenSupplier);
+        return new ElapsedTimeBeater(sleepingPeriodInMillis, edenSupplier);
     }
 
-    public void addSubscriber(Subscriber subscriber) {
-        this.subscribersEden.addSubscriber(subscriber);
+    public boolean addSubscriber(Subscriber subscriber) {
+        return this.subscribersEden.addSubscriber(subscriber);
     }
 
     public void removeSubscriber(Subscriber subscriber) {
@@ -38,17 +63,14 @@ public class ElapsedTimeBeater implements Runnable {
         return sleepingPeriodInMillis;
     }
 
-
+    @Override
     public void run( ) {
-        while (true) {
-            try {
-                Thread.sleep(this.sleepingPeriodInMillis);
-                this.subscribersEden.notifySubscribers();
+        this.subscribersEden.notifySubscribers();
+    }
 
-            } catch (InterruptedException e) {
-                this.subscribersEden.onClose();
-                return;
-            }
-        }
+    @Override
+    public void onClose( ) {
+        this.subscribersEden.onClose();
+        this.timeBeat.shutdownNow();
     }
 }
