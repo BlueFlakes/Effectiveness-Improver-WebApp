1dcf5483fd2aa8a6dbc464a869e99bec176fefed
Kamil <blueflamesxcode@gmail.com>
2018-03-19 12:32:35 +0100

    Add ContainerExceededBoundaries exception for being more flexible than too specific similar exceptions

---
+++ src/main/java/com/webapp/effectiveness/common/exceptions/ContainerExceededBoundariesException.java
@@ -0,0 +1,6 @@
+package com.webapp.effectiveness.common.exceptions;
+
+import com.webapp.effectiveness.baseclasses.ApplicationRuntimeException;
+
+public class ContainerExceededBoundariesException extends ApplicationRuntimeException{
+}
